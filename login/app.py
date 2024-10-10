dac3b60ca1dc5e64d9b2b66823ee262c1c0b1729
Imran Salah <imransalah@icloud.com>
2024-10-10 18:18:21 +1100

    Added min password length, added logo and encryption

--- login/app.py
+++ login/app.py
@@ -3,6 +3,7 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 from dashboard import create_modelevaluation_dashboards, create_modelcomparison_dashboard
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -15,7 +16,7 @@ app.secret_key = 'your secret key'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '@#Toyon123'
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'logintest'
 
 mysql = MySQL(app)
@@ -29,18 +30,18 @@ def login():
         username = request.form['username']
         password = request.form['password']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(
-            'SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username,))
         account = cursor.fetchone()
-        if account:
+        if account and check_password_hash(account['password'], password):
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
-            msg = 'Logged in successfully !'
+            msg = 'Logged in successfully!'
             return redirect('/index')
         else:
-            msg = 'Incorrect username / password !'
+            msg = 'Incorrect username / password!'
     return render_template('login.html', msg=msg)
+
 
 
 @app.route('/logout')
@@ -59,26 +60,27 @@ def register():
         password = request.form['password']
         email = request.form['email']
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute(
-            'SELECT * FROM accounts WHERE username = % s', (username, ))
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username,))
         account = cursor.fetchone()
         if account:
-            msg = 'Account already exists !'
+            msg = 'Account already exists!'
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address !'
+            msg = 'Invalid email address!'
+        elif len(password) < 8:
+            msg = 'Password must be 8 characters long!'
         elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers !'
+            msg = 'Username must contain only characters and numbers!'
         elif not username or not password or not email:
-            msg = 'Please fill out the form !'
+            msg = 'Please fill out the form!'
         else:
-            cursor.execute(
-                'INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            # Hash the password before storing
+            hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, hashed_password, email,))
             mysql.connection.commit()
-            msg = 'You have successfully registered !'
+            msg = 'You have successfully registered!'
     elif request.method == 'POST':
-        msg = 'Please fill out the form !'
+        msg = 'Please fill out the form!'
     return render_template('register.html', msg=msg)
-
 
 @app.route('/index')
 def index():
