d3f5166deb7379e217d17860753aeffd4179b3b3
Mohammad Tousif Shahriar <toyonmohammad10@gmail.com>
2024-11-14 01:32:23 +1100

    fixed scatter error

--- login/dashboard/__init__.py
+++ login/dashboard/__init__.py
@@ -15,12 +15,11 @@ def create_modelevaluation_dashboards(server):
 def create_modelcomparison_dashboard(server):
     def load_dashboard(df, model, server, url, column_name):
         df['Predicted'] = df[column_name]
-        return ModelComparisonDashboard(df, model, server, url)
+        return ModelComparisonDashboard(df.dropna(subset=['Predicted']), model, server, url)
     
     df1 = pd.read_parquet("dashboard/building1_predicted.parquet")
     df2 = pd.read_parquet("dashboard/building2_predicted.parquet")
     df3 = pd.read_parquet("dashboard/building3_predicted.parquet")
-    
     model_rfc1 = load('dashboard/models/randomforest_building1_tuned.joblib')
     model_rfc2 = load('dashboard/models/randomforest_building2_tuned.joblib')
     model_rfc3 = load('dashboard/models/randomforest_building3_tuned.joblib')
